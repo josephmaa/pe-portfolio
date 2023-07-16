dcbedc0c3d315fa561edfd436e88d9687d57aa85
Joseph Maa <josephgmaa@berkeley.edu>
2023-07-15 21:50:24 -0700

    Update redeploy to use systemctl service

--- redeploy-script.sh
+++ redeploy-script.sh
@@ -4,3 +4,6 @@ cd pe-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
