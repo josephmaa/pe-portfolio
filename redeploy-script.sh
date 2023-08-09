d1fbfd7aad4f043be78475d1dcce0cbbcaf51330
Joseph Maa <josephgmaa@berkeley.edu>
2023-08-09 14:30:36 -0700

    Update script

--- redeploy-script.sh
+++ redeploy-script.sh
@@ -4,6 +4,5 @@ cd pe-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
